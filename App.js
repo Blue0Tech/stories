f7e37af92ea3ff6d53fa39f83f08fe1889e1dce1
Pruthvi Shrikaanth <shrikaanthpruthvispareemail@gmail.com>
2020-06-17 15:43:30 +0100

    Added icons to navigation

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import readScreen from './screens/readScreen';
 import writeScreen from './screens/writeScreen';
+import readIcon from './assets/IconRead64px.png';
+import writeIcon from './assets/IconWrite64px.png';
 
 export default function App() {
   return (
@@ -12,8 +14,20 @@ export default function App() {
 }
 
 const tabNavigator = createBottomTabNavigator({
-  Read : {screen : readScreen},
-  Write : {screen : writeScreen}
+  Read : {screen : readScreen, navigationOptions : {
+    title : 'Read stories',
+    tabBarLabel : 'Read stories',
+    tabBarIcon : () => (
+      <Image source={readIcon} style={{ width : 20, height : 20 }}/>
+    )
+  }},
+  Write : {screen : writeScreen, navigationOptions : {
+    title : 'Write stories',
+    tabBarLabel : 'Write stories',
+    tabBarIcon : () => (
+      <Image source={writeIcon} style={{ width : 20, height : 20 }}/>
+    )
+  }}
 });
 
 const AppContainer = createAppContainer(tabNavigator);
