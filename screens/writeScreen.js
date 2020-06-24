88e7df3d84adf6aac27d4a897b68527ddae508ec
Pruthvi Shrikaanth <shrikaanthpruthvispareemail@gmail.com>
2020-06-24 22:42:56 +0100

    Added toast and keyboard avoiding overlay functionality

--- screens/writeScreen.js
+++ screens/writeScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View, TextInput, Dimensions, TouchableOpacity } from 'react-native';
+import { Text, View, TextInput, Dimensions, TouchableOpacity, Platform, KeyboardAvoidingView, ToastAndroid } from 'react-native';
 import AsyncStorage from '@react-native-community/async-storage';
 import db from '../config';
 
@@ -34,79 +34,85 @@ export default class writeScreen extends React.Component {
 			timestamp : timestamp
 		});
 	}
+	displayToast=()=>{
+		ToastAndroid.showWithGravity("Your story has been submitted!",ToastAndroid.SHORT,ToastAndroid.TOP);
+	}
 	render() {
+		const behavior = Platform.OS === "ios" ? "position" : "";
 		return (
-			<View style={{flex : 1, alignContent : 'center'}}>
-				<TextInput
-					style = {{
-						width : Dimensions.get('window').width*0.9,
-						borderWidth : 2,
-						borderColor : '#000000',
-						alignSelf : 'center',
-						margin : 10
-					}}
-					onChangeText = {
-						text => {
-							this.setState({ author : text });
+				<KeyboardAvoidingView style={{flex : 1, alignContent : 'center'}} behavior={behavior} enabled>
+					<TextInput
+						style = {{
+							width : Dimensions.get('window').width*0.9,
+							borderWidth : 2,
+							borderColor : '#000000',
+							alignSelf : 'center',
+							margin : 10,
+							marginTop : Dimensions.get('window').height*0.15
+						}}
+						onChangeText = {
+							text => {
+								this.setState({ author : text });
+							}
 						}
-					}
-					value = {
-						this.state.author
-					}
-				/>
-				<TextInput
-					style = {{
-						width : Dimensions.get('window').width*0.9,
-						borderWidth : 2,
-						borderColor : '#000000',
-						alignSelf : 'center',
-						margin : 10
-					}}
-					onChangeText = {
-						text => {
-							this.setState({ name : text });
+						value = {
+							this.state.author
 						}
-					}
-					value = {
-						this.state.name
-					}
-				/>
-				<TextInput
-					multiline = {true}
-					style = {{
-						width : Dimensions.get('window').width*0.9,
-						height : Dimensions.get('window').height*0.4,
-						borderWidth : 2,
-						borderColor : '#000000',
-						alignSelf : 'center',
-						margin : 10
-					}}
-					onChangeText = {
-						text => {
-							this.setState({ story : text });
+					/>
+					<TextInput
+						style = {{
+							width : Dimensions.get('window').width*0.9,
+							borderWidth : 2,
+							borderColor : '#000000',
+							alignSelf : 'center',
+							margin : 10
+						}}
+						onChangeText = {
+							text => {
+								this.setState({ name : text });
+							}
 						}
-					}
-					value = {
-						this.state.story
-					}
-				/>
-				<TouchableOpacity
-					style = {{
-						alignSelf : 'center',
-						width : Dimensions.get('window').width*0.4,
-						height : 20,
-						alignContent : 'center',
-						justifyContent : 'center',
-						backgroundColor : '#a8b61e'
-					}}
-					onPress = {()=>{/*this.addStory(this.state.story)*/
-						var timestamp =  Date(); //I could not find a server to get a timestamp, so I had to use the local version
-						this.addStoryOnline(this.state.author,this.state.name,this.state.story,timestamp);
-					}}
-				>
-					<Text style={{alignSelf : 'center'}}>Submit</Text>
-				</TouchableOpacity>
-			</View>
+						value = {
+							this.state.name
+						}
+					/>
+					<TextInput
+						multiline = {true}
+						style = {{
+							width : Dimensions.get('window').width*0.9,
+							height : Dimensions.get('window').height*0.4,
+							borderWidth : 2,
+							borderColor : '#000000',
+							alignSelf : 'center',
+							margin : 10
+						}}
+						onChangeText = {
+							text => {
+								this.setState({ story : text });
+							}
+						}
+						value = {
+							this.state.story
+						}
+					/>
+					<TouchableOpacity
+						style = {{
+							alignSelf : 'center',
+							width : Dimensions.get('window').width*0.4,
+							height : 20,
+							alignContent : 'center',
+							justifyContent : 'center',
+							backgroundColor : '#a8b61e'
+						}}
+						onPress = {async()=>{/*this.addStory(this.state.story)*/
+							var timestamp =  Date.now();
+							await this.addStoryOnline(this.state.author,this.state.name,this.state.story,timestamp);
+							this.displayToast();
+						}}
+					>
+						<Text style={{alignSelf : 'center'}}>Submit</Text>
+					</TouchableOpacity>
+				</KeyboardAvoidingView>
 		)
 	}
 }
